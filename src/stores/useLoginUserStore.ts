b37df7c98e37efd1284775c42686ef0cdbfb2713
Today <xiaoqianliya@gmail.com>
2025-03-01 22:51:05 +0800

    用户模块

--- src/stores/useLoginUserStore.ts
+++ src/stores/useLoginUserStore.ts
@@ -1,20 +1,23 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import { getLoginUserUsingGet } from '@/api/userController.ts'
 
 /**
  * 存储登录用户信息的状态
  */
 export const useLoginUserStore = defineStore('loginUser', () => {
-  const loginUser = ref<any>({
+  const loginUser = ref<API.LoginUserVO>({
     userName: '未登录',
   })
 
+  /**
+   * 远程获取登录用户信息
+   */
   async function fetchLoginUser() {
-    //todo 未提供接口
-    //测试用户登录
-    setTimeout(() => {
-      loginUser.value = { userName: '测试用户', id: 1 }
-    }, 3000)
+    const res = await getLoginUserUsingGet()
+    if (res.data.code === 0 && res.data.data) {
+      loginUser.value = res.data.data
+    }
   }
 
   /**
